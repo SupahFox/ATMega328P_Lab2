0b9899451755aad137c0ba2a9395d585142e4988
SupahFox <31490939+SupahFox@users.noreply.github.com>
2023-09-19 20:12:53 -0300

    comments

--- Lab2/main.asm
+++ Lab2/main.asm
@@ -14,39 +14,39 @@
 
 ; Empiezo con los vectores de interrupción
 .ORG 0x0000
-	jmp		start		;dirección de comienzo (vector de reset)
+	jmp		start		; dirección de comienzo (vector de reset)
 .org	0x0008 
 	jmp		interrupcion_externa 
 .ORG 0x001C 
-	jmp		_tmr0_int	;salto atención a rutina de comparación A del timer 0
+	jmp		_tmr0_int	; salto atención a rutina de comparación A del timer 0
 
 
 ; ---------------------------------------------------------------------------------------
 ; acá empieza el programa
 start:
-;configuro los puertos:
+; configuro los puertos:
 ;	PB2 PB3 PB4 PB5	- son los LEDs del shield
     ldi		r16,	0b00111101	
-	out		DDRB,	r16			;4 LEDs del shield son salidas
-	out		PORTB,	r16			;apago los LEDs
+	out		DDRB,	r16			; 4 LEDs del shield son salidas
+	out		PORTB,	r16			; apago los LEDs
 
 	ldi		r16,	0b00000000	
-	out		DDRC,	r16			;3 botones del shield son entradas
-;-------------------------------------------------------------------------------------
+	out		DDRC,	r16			; 3 botones del shield son entradas
+; -------------------------------------------------------------------------------------
 
-;Configuro el TMR0 y su interrupcion.
+; Configuro el TMR0 y su interrupcion.
 	ldi		r16,	0b00000010	
-	out		TCCR0A,	r16			;configuro para que cuente hasta OCR0A y vuelve a cero (reset on compare), ahí dispara la interrupción
+	out		TCCR0A,	r16			; configuro para que cuente hasta OCR0A y vuelve a cero (reset on compare), ahí dispara la interrupción
 	ldi		r16,	0b00000101	
-	out		TCCR0B,	r16			;prescaler = 1024
+	out		TCCR0B,	r16			; prescaler = 1024
 	ldi		r16,	124	
-	out		OCR0A,	r16			;comparo con 124
+	out		OCR0A,	r16			; comparo con 124
 	ldi		r16,	0b00000010	
-	sts		TIMSK0,	r16			;habilito la interrupción (falta habilitar global)
-;-------------------------------------------------------------------------------------
-;Inicializo algunos registros que voy a usar como variables.
-	ldi		r24,	0x00		;inicializo r24 para un contador genérico
-;-------------------------------------------------------------------------------------
+	sts		TIMSK0,	r16			; habilito la interrupción (falta habilitar global)
+; -------------------------------------------------------------------------------------
+; Inicializo algunos registros que voy a usar como variables.
+	ldi		r24,	0x00		 ; inicializo r24 para un contador genérico
+; -------------------------------------------------------------------------------------
 ldi		r16, 0b00000010
 sts		PCICR,r16
 ldi		r16, 0b00000100
@@ -54,10 +54,10 @@ sts		PCMSK1,r16
 
 
 
-;Programa principal ... acá puedo hacer lo que quiero
+; Programa principal ... acá puedo hacer lo que quiero
 
 comienzo:
-	sei							;habilito las interrupciones globales(set interrupt flag)
+	sei							; habilito las interrupciones globales(set interrupt flag)
 
 loop1:
 	nop
@@ -68,7 +68,7 @@ loop1:
 	nop
 	nop
 	nop
-	brne	loop1  //branch not equal 0 la zero flag es 0 salta si la operacion anterior da cero por ende va a loop 2
+	brne	loop1  ; branch not equal 0 la zero flag es 0 salta si la operacion anterior da cero por ende va a loop 2
 loop2:
 	nop
 	nop
@@ -90,23 +90,23 @@ fin:
 ; Por ahora solo: cambia el valor de un LED en la placa, e incrementa un contador en r24.
 
 _tmr0_int:
-	in	    r17,SREG 			
+	in	    r17, SREG 			
 	inc		r24	
-	cpi		r24,125
+	cpi		r24, 125
 	breq	cambio_led	
 	rjmp	_tmr0_out
 
 cambio_led:	
-	sbi PINB, 2			;cambio LED
+	sbi PINB, 2			; cambio LED
 
 _tmr0_out:
 	out		SREG,r17	   
-	reti						;retorno de la rutina de interrupción del Timer0
+	reti						; retorno de la rutina de interrupción del Timer0
 
 interrupcion_externa:			
 	in	    r17,SREG 
 	sbi		PINB, 5	
-						;toggle LED camvia de estado D1
-	inc		r24			;cuento cuántas veces entré en la rutina.
+						; toggle LED camvia de estado D1
+	inc		r24			; cuento cuántas veces entré en la rutina.
 	out		SREG,r17
 	reti
